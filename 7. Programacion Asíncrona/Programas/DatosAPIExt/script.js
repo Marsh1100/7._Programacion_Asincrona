e9870b19adff8aecdd22e5c75066c57d5a0b22c1
Marsh1100 <margie.b.agu17@gmail.com>
2023-07-06 17:18:31 -0500

    feat: :sparkles: Api clima

--- 7. Programacion Asíncrona/Programas/DatosAPIExt/script.js
+++ 7. Programacion Asíncrona/Programas/DatosAPIExt/script.js
@@ -1,8 +1,13 @@
-async function fetchData() {
+//jQuery
+$(document).ready(function() {
+  $('.js-example-basic-single').select2();
+});
+
+async function fetchData(ciudad) {
     try {
     //   const response = await fetch('https://api.example.com/data');
     //const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=4&lon=72&appid=961ba900fa9486fe75a948a5579b0891");
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=961ba900fa9486fe75a948a5579b0891`);
     
       const data = await response.json();
       return data;
@@ -10,9 +15,9 @@ async function fetchData() {
       throw 'Error al obtener los datos';
     }
   }
-  async function displayData() {
+  async function displayData(ciudad) {
     try {
-      const data = await fetchData();
+      const data = await fetchData(ciudad);
       const dataContainer = document.getElementById('data-container');
       visualizar(data)
       //dataContainer.innerText = JSON.stringify(data);
@@ -24,22 +29,47 @@ async function fetchData() {
       console.error(error);
     }
   }
-  displayData();
+//displayData("bogota");
+
+
   
 //Elementos del DOM
+const $selector = document.getElementById('selector');
+
+const $icon = document.getElementById('icon')
 const $coordenadas = document.getElementById('coordenadas');
 const $temp = document.getElementById('temp');
 const $pressure = document.getElementById('pressure');
 const $humidity = document.getElementById('humidity');
 
-
+//Funciones
+$selector.addEventListener('change',(e) =>{
+  //console.log("holaa")
+  let ciudad = e.target.value;
+  displayData(ciudad);
+})
 function visualizar(data){
   console.log(data.lenght);
 
+  //ICON
+  let img = `<img src=" https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" ></img>`
+  $icon.innerHTML = img;
   //Coordenadas
-  let coord = `<p>Latitud: ${data.coord.lat}</p>
-              <p>Longitud: ${data.coord.lon}</p>`;
-  $coordenadas.insertAdjacentHTML('beforeend',coord);
-
+  let coord = `Latitud: ${data.coord.lat}
+               Longitud: ${data.coord.lon}`;
+  $coordenadas.innerHTML= coord;
+ 
   //Temperatura
+  let dataTem = (data.main.temp-273.15).toFixed(1);
+  let temp = `<p>${dataTem} ºC</p>`;
+  $temp.innerHTML=temp;
+
+  //Presión
+  let pressure = `<p>${(data.main.pressure/1000).toFixed(2)} atm</p>`;
+  $pressure.innerHTML=pressure;
+
+  //Humedad
+  let humidity = `<p>${data.main.humidity} %</p>`;
+  $humidity.innerHTML=humidity;
+
 } 
